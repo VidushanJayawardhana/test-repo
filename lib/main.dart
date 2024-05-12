3f69b898c741b7892cf13e9b9a4398ecafc6e701
VidushanJayawardhana <vidushanjayawardhane99@gmail.com>
2024-05-12 23:11:46 +0530

    first commit git

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: [Text("Denuka",style: TextStyle(fontSize: 36,),), Text("Hi"),],
+          ),
+        ),
+      ),
+    );
+  }
+}
